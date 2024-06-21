b7339ee111e88452d3b6c5303a5437582c09eb85
Daniil10Sh <danik2022odessa@gmail.com>
2024-06-21 16:11:52 +0300

    add changes

---
+++ task-1.js
@@ -0,0 +1,8 @@
+// const categories = document.querySelectorAll('#categories .item');
+// console.log(`В списку ${categories.length} категорії.`);
+// categories.forEach(category => {
+//     const categoryTitle = category.querySelector('h2').textContent;
+//     const categoryItemsCount = category.querySelectorAll('ul li').length;
+//     console.log(`Категорія: ${categoryTitle}, Кількість елементів: ${categoryItemsCount}`);
+// });
+
--- task-2.js
+++ task-2.js
@@ -1,11 +1,3 @@
-// const categories = document.querySelectorAll('#categories .item');
-// console.log(`В списку ${categories.length} категорії.`);
-// categories.forEach(category => {
-//     const categoryTitle = category.querySelector('h2').textContent;
-//     const categoryItemsCount = category.querySelectorAll('ul li').length;
-//     console.log(`Категорія: ${categoryTitle}, Кількість елементів: ${categoryItemsCount}`);
-// });
-
 
 
 // const ingredients = [
