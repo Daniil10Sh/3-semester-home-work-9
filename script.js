c99ba787fa1c28bf0611ace7c191eb97e5564663
Daniil10Sh <danik2022odessa@gmail.com>
2024-06-21 16:00:35 +0300

    fixtion  of html and js

---
+++ script.js
@@ -0,0 +1,26 @@
+// const categories = document.querySelectorAll('#categories .item');
+// console.log(`В списку ${categories.length} категорії.`);
+// categories.forEach(category => {
+//     const categoryTitle = category.querySelector('h2').textContent;
+//     const categoryItemsCount = category.querySelectorAll('ul li').length;
+//     console.log(`Категорія: ${categoryTitle}, Кількість елементів: ${categoryItemsCount}`);
+// });
+
+
+
+// const ingredients = [
+//     'Картошка',
+//     'Грибы',
+//     'Чеснок',
+//     'Помидоры',
+//     'Зелень',
+//     'Приправы',
+//   ];
+//   const ingredientsList = document.getElementById('ingredients');
+//   const ingredientsItems = ingredients.map(ingredient => {
+//     const li = document.createElement('li');
+//     li.textContent = ingredient;
+//     return li;
+//   });
+//   ingredientsList.append(...ingredientsItems);
+  
